f4a8dcdba9e5cfbccc750143a13985da316cf59c
kevin olson <acidjazz@gmail.com>
2015-11-06 18:44:06 -0800

    shebang

--- cof/monitor.coffee
+++ cof/monitor.coffee
@@ -1,3 +1,4 @@
+`#!/usr/bin/env node`
 
 path = process.cwd()+'/'
 pub = path+'pub/'
--- pub/jst/monitor.js
+++ pub/jst/monitor.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 var exts, fs, path, pub, render, throttles,
   indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
